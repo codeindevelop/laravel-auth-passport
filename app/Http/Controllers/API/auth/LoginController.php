da099596085b2448738f5099c53880b7f4bfc0d0
codeindev <codeindevelopment@gmail.com>
2022-06-22 15:33:03 +0430

    implement login

--- app/Http/Controllers/API/auth/LoginController.php
+++ app/Http/Controllers/API/auth/LoginController.php
@@ -3,9 +3,66 @@
 namespace App\Http\Controllers\API\auth;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'email' => ['email', 'required'],
+            'password' => ['required']
+        ]);
+
+        // If validator has ben error
+        if ($validate->fails()) {
+            return response()->json([
+                'message' => $validate->errors()->first()
+            ]);
+        }
+
+        $cerdinentals = request(['email', 'password']);
+
+        // If user dos not exist
+        if (!Auth::attempt($cerdinentals)) {
+            return response()->json([
+                'message' => 'unauthorized'
+            ], 401);
+        }
+
+        $user = Auth::user();
+
+        // If user email has not verified return this message
+        if (!$user->hasVerifiedEmail()) {
+            return response()->json([
+                'message' => 'Email has not verified'
+            ], 401);
+        }
+
+        $tokenResult = $user->createToken('Login Token');
+        $token = $tokenResult->token;
+
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse(
+                $tokenResult->token->expires_at
+            )->toDateTimeString(),
+
+        ], 200);
+    }
+
+
+    //Show user profile detaile
+    public function profile()
+    {
+        $user = Auth::user();
+
+        return response()->json([
+            'user' => $user
+        ], 200);
+    }
 }
--- routes/API/auth/auth.php
+++ routes/API/auth/auth.php
@@ -1,17 +1,28 @@
 <?php
 
+use App\Http\Controllers\API\auth\LoginController;
 use App\Http\Controllers\API\auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-
+// Public Routes
 Route::prefix('v1')->group(function () {
 
     // Regitser user API
-    Route::post('register', [RegisterController::class,'register']);
+    Route::post('register', [RegisterController::class, 'register']);
 
 
+    // API for user active email
     Route::get('activation/{token}', [RegisterController::class, 'signupActive']);
 
+    // API for user login
+    Route::post('login', [LoginController::class, 'login']);
+
+    // Private Routes
+    Route::middleware("auth:api")->group(function () {
+
+        // Show user profile
+        Route::get('profile', [LoginController::class, 'profile']);
+    });
 });
